203b5a2486b1d91686d68e973271344661ba362b
Benjamin Trout <benjamin.c.trout@gmail.com>
2023-12-04 10:40:54 -0500

    DB Updates

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,9 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS recipe;
+DROP TABLE IF EXISTS recipe_ingredient;
+DROP TABLE IF EXISTS ingredient;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +11,28 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE recipe (
+	recipe_id SERIAL NOT NULL,
+	recipe_name varchar(256) NOT NULL,
+	recipe_instructions varchar(400) NOT NULL,
+	constraint PK_recipe PRIMARY KEY (recipe_id)
+);
+
+CREATE TABLE ingredient (
+	ingredient_id serial NOT NULL,
+	ingredient_name varchar(200) NOT NULL,
+	CONSTRAINT PK_ingredient_id PRIMARY KEY(ingredient_id),
+	CONSTRAINT UQ_ingredient_name UNIQUE(ingredient_name)
+);
+
+CREATE TABLE recipe_ingredient (
+	recipe_id int,
+    ingredient_id int,
+    CONSTRAINT PK_recipe_ingredient PRIMARY KEY (recipe_id, ingredient_id),
+    CONSTRAINT FK_recipe_id FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id),
+    CONSTRAINT FK_ingredient_id FOREIGN KEY (ingredient_id) REFERENCES ingredient(ingredient_id)
 );
 
 COMMIT TRANSACTION;
