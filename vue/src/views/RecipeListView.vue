dce23c7cc5767c5b4f04c155c917c28e89cb0cf5
Steven <coolfire719@yahoo.com>
2023-12-07 13:30:18 -0500

    Fixed import routes and deleted not needed view vue's

--- vue/src/views/RecipeListView.vue
+++ vue/src/views/RecipeListView.vue
@@ -6,7 +6,7 @@
 
 <script>
 import RecipeListPage
-  from "../../../../../../../Pictures/team-november-final-capstone-3f0c4ce0bfa6ff2405f01ba3c0a9141c3de0a1ca/vue/src/components/RecipeListPage.vue";
+  from "../components/RecipeListPage.vue";
 
 export default {
   components: {
