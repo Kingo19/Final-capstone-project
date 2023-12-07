f8892aeedcbb61f0d7fd1f5de7fae8f2a102dd28
Steven <coolfire719@yahoo.com>
2023-12-07 15:16:32 -0500

    up

--- vue/src/views/RegisterView.vue
+++ vue/src/views/RegisterView.vue
@@ -2,25 +2,33 @@
   <div id="register" class="text-center">
     <form v-on:submit.prevent="register">
       <h1>Create Account</h1>
+
+
       <div role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
       </div>
+
       <div class="form-input-group">
         <label for="username">Username</label>
         <input type="text" id="username" v-model="user.username" required autofocus/>
       </div>
+
       <div class="form-input-group">
         <label for="password">Password</label>
         <input type="password" id="password" v-model="user.password" required/>
       </div>
+
       <div class="form-input-group">
         <label for="confirmPassword">Confirm Password</label>
         <input type="password" id="confirmPassword" v-model="user.confirmPassword" required/>
       </div>
+
       <button type="submit">Create Account</button>
+
       <p>
         <router-link v-bind:to="{ name: 'login' }">Already have an account? Log in.</router-link>
       </p>
+
     </form>
   </div>
 </template>
