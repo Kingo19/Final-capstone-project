eb578081b8f8ba1c6df0890c6cf1f2659668afde
Benjamin Trout <benjamin.c.trout@gmail.com>
2023-12-12 12:22:11 -0500

    Plan get by user

--- java/src/main/java/com/techelevator/dao/DailyPlanDao.java
+++ java/src/main/java/com/techelevator/dao/DailyPlanDao.java
@@ -3,7 +3,10 @@ package com.techelevator.dao;
 import com.techelevator.model.DailyPlan;
 import com.techelevator.model.DailyPlanDto;
 
+import java.util.List;
+
 public interface DailyPlanDao {
     public DailyPlan createDailyPlan(DailyPlanDto dailyPlanDto, int userId);
     public DailyPlan getPlanByDate(String date, int userId);
+    public List<DailyPlan> getAllUserPlans(int userId);
 }
